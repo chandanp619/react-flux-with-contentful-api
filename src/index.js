493bd5100ccb051f7fe33029c392e51aa1a6c0e5
Chandan Pradhan <chandanp619@gmail.com>
2021-10-08 22:20:23 +0530

    removed console log

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 
-console.log(process.env.SPACE_ID);
 ReactDOM.render(<App />, document.getElementById('App'));
